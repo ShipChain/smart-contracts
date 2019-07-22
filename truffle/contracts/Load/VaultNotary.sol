0d5c91cce1ce59da326058f3b3402840963229af
jianwel <ljw725@gmail.com>
2019-07-22 14:38:24 -0400

    changed to use my own acl control

--- truffle/contracts/Load/VaultNotary.sol
+++ truffle/contracts/Load/VaultNotary.sol
@@ -1,66 +1,75 @@
 pragma solidity 0.5.0;
 
-import {WhitelistAdminRole} from "openzeppelin-solidity/contracts/access/roles/WhitelistAdminRole.sol";
-import {WhitelistedRole} from "openzeppelin-solidity/contracts/access/roles/WhitelistedRole.sol";
+
 import {Ownable} from "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import {SingleNotary} from "./lib/SingleNotary.sol";
 
 
-contract SingleNotary is WhitelistedRole {
-    struct Data {
-        string vaultHash;
-        string vaultURI;
+contract VaultNotary is Ownable {
+
+    mapping(bytes16 => SingleNotary.Data) private notaryMapping;
+    mapping(bytes16 => address[]) private aclMapping;
+
+
+    //if found return the index in the range of [0, addressList.length-1]
+    //if not found, return addressList.length
+    function findIndex(address toCheck, address[] memory addressList)
+    internal
+    pure
+    returns (uint256) {
+        uint256 i;
+        for (i = 0; i < addressList.length; i++) {
+            if (toCheck == addressList[i])
+                return i;
+        }
+        return i;
     }
 
-    function revokeUpdatePermissionForOneVault(address anotherAddress) onlyWhitelistAdmin external {
+    function deleteByValue(address toCheck, address[] storage addressList)
+    internal {
+        require(addressList.length > 0);
+        uint256 index = findIndex(toCheck, addressList);
+        if (index != addressList.length) {
+            addressList[index] = addressList[addressList.length - 1];
+            delete addressList[addressList.length - 1];
+        }
+    }
+
+    modifier whitelistedOnly(bytes16 vaultId) {
+        require(findIndex(msg.sender, aclMapping[vaultId]) != aclMapping[vaultId].length);
+        _;
+    }
+
+    function registerVault(bytes16 vaultId, string calldata vaultURI, string calldata vaultHash)
+    external {
+        aclMapping[vaultId].push(address(msg.sender));
+        setVaultURI(vaultId, vaultURI);
+        setVaultHash(vaultId, vaultHash);
+    }
+
+    function grantUpdatePermission(bytes16 vaultId, address anotherAddress)
+    external
+    onlyOwner {
+        aclMapping[vaultId].push(anotherAddress);
 
     }
 
-    function grantUpdatePermissionForOneVault(address anotherAddress) onlyWhitelistAdmin external {
-
+    function revokeUpdatePermission(bytes16 vaultId, address anotherAddress)
+    external
+    onlyOwner {
+        deleteByValue(anotherAddress, aclMapping[vaultId]);
     }
 
-    function setVaultURIForOneVault(string calldata vaultURI) onlyWhitelisted external {
-        self.vaultURI = vaultURI;
+    function setVaultURI(bytes16 vaultId, string memory vaultURI)
+    public
+    whitelistedOnly(vaultId) {
+        notaryMapping[vaultId].vaultURI = vaultURI;
     }
 
-}
-
-
-contract VaultNotary {
-    SingleNotary[] notaryArray;
-    uint256 count =0;
-
-    mapping (bytes16 => uint256) private addressToArrayIndex;
-    function getNotary(bytes16 vaultId) {
-        return notaryArray[addressToArrayIndex[vaultId]];
-    }
-
-
-    function registerVault(bytes16 vaultId, string calldata vaultURI, string calldata vaultHash) external {
-        newNotary = new SingleNotary();
-        count = count + 1;
-        notaryArray.push(newNotary);
-        addressToArrayIndex[vaultId] = count;
-
-        getNotary(vaultId).addWhitelistAdmin(msg.sender);
-        emit allNotaryData[vaultId].WhitelistAdminAdded(msg.sender);
-    }
-
-    function grantUpdatePermission(bytes16 vaultId, address anotherAddress) external {
-        allNotaryData[vaultId].grantUpdatePermissionForOneVault(anotherAddress);
-
-    }
-
-    function revokeUpdatePermission(bytes16 vaultId, address anotherAddress) external {
-        allNotaryData[vaultId].invokeUpdatePermissionForOneVault(anotherAddress);
-    }
-
-    function setVaultURI(bytes16 vaultId, string calldata vaultURI) external {
-        setVaultURIForOneVault(vaultURI);
-    }
-
-    function setVaultHash(bytes16 vaultId, string calldata vaultHash) external {
-
+    function setVaultHash(bytes16 vaultId, string memory vaultHash)
+    public
+    whitelistedOnly(vaultId) {
+        notaryMapping[vaultId].vaultHash = vaultHash;
     }
 
 }
---
+++ truffle/contracts/Load/lib/SingleNotary.sol
@@ -0,0 +1,12 @@
+pragma solidity 0.5.0;
+
+library SingleNotary {
+    event VaultUri(address indexed msgSender, bytes16 indexed shipmentUuid, string vaultUri);
+    event VaultHash(address indexed msgSender, bytes16 indexed shipmentUuid, string vaultHash);
+
+    struct Data {
+        string vaultHash;
+        string vaultURI;
+    }
+
+}
