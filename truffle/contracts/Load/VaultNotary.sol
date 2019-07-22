8acfd4cbc275043083606890798838337829c4d6
jianwel <ljw725@gmail.com>
2019-07-22 16:36:55 -0400

    in the middle of change,  TypeError: Library cannot have non-constant state variables

--- truffle/contracts/Load/VaultNotary.sol
+++ truffle/contracts/Load/VaultNotary.sol
@@ -6,75 +6,77 @@ import {SingleNotary} from "./lib/SingleNotary.sol";
 
 
 contract VaultNotary is Ownable {
+   // using SingleNotary for SingleNotary.Data;
+    //using SingleNotary for SingleNotary.singleAclMapping;
 
-    mapping(bytes16 => SingleNotary.Data) private notaryMapping;
-    mapping(bytes16 => address[]) private aclMapping;
+    mapping(bytes16 => SingleNotary) private notaryMapping;
+//    mapping(bytes16 => SingleNotary.singleAclMapping) private aclMapping;
 
-        // Vault Events
+    bool private isDeprecated; //1 byte
+
+
+
+    // Notary Events
     event VaultUri(address indexed msgSender, bytes16 indexed vaultId, string vaultUri);
     event VaultHash(address indexed msgSender, bytes16 indexed vaultId, string vaultHash);
+    event ContractDeprecatedSet(address indexed msgSender, bool isDeprecated);
 
     modifier whitelistedOnly(bytes16 vaultId) {
-        require(findIndex(msg.sender, aclMapping[vaultId]) != aclMapping[vaultId].length);
+        require(notaryMapping[vaultId].aclMapping[msg.sender]);
         _;
+    }
+
+    modifier vaultOwnerOnly(bytes16 vaultId) {
+        require(msg.sender == notaryMapping[vaultId].vaultOwner);
+        _;
+    }
+
+    modifier notDeprecated() {
+        require(!isDeprecated, "This version of the VaultNotary contract has been deprecated");
+        _;
+    }
+
+    function setDeprecated(bool _isDeprecated)
+    external
+    onlyOwner
+    {
+        isDeprecated = _isDeprecated;
+        emit ContractDeprecatedSet(msg.sender, isDeprecated);
     }
 
     function registerVault(bytes16 vaultId, string calldata vaultUri, string calldata vaultHash)
     external {
-        aclMapping[vaultId].push(address(msg.sender));
+        notaryMapping[vaultId].vaultOwner = msg.sender;
+        notaryMapping[vaultId].aclMapping[msg.sender] = true;
         setVaultUri(vaultId, vaultUri);
         setVaultHash(vaultId, vaultHash);
     }
 
     function grantUpdatePermission(bytes16 vaultId, address anotherAddress)
     external
-    onlyOwner {
-        aclMapping[vaultId].push(anotherAddress);
-
+    vaultOwnerOnly(vaultId) {
+        notaryMapping[vaultId].aclMapping[msg.sender] = true;
     }
 
     function revokeUpdatePermission(bytes16 vaultId, address anotherAddress)
     external
-    onlyOwner {
-        deleteByValue(anotherAddress, aclMapping[vaultId]);
+    vaultOwnerOnly(vaultId) {
+        notaryMapping[vaultId].aclMapping[msg.sender] = false;
     }
 
     function setVaultUri(bytes16 vaultId, string memory vaultUri)
     public
     whitelistedOnly(vaultId) {
-        notaryMapping[vaultId].vaultUri = vaultUri;
+        notaryMapping[vaultId].Data.vaultUri = vaultUri;
         emit VaultUri(msg.sender, vaultId, vaultUri);
     }
 
     function setVaultHash(bytes16 vaultId, string memory vaultHash)
     public
     whitelistedOnly(vaultId) {
-        notaryMapping[vaultId].vaultHash = vaultHash;
+        notaryMapping[vaultId].Data.vaultHash = vaultHash;
         emit VaultHash(msg.sender, vaultId, vaultHash);
     }
 
-    //if found return the index in the range of [0, addressList.length-1]
-    //if not found, return addressList.length
-    function findIndex(address toCheck, address[] memory addressList)
-    internal
-    pure
-    returns (uint256) {
-        uint256 i;
-        for (i = 0; i < addressList.length; i++) {
-            if (toCheck == addressList[i])
-                return i;
-        }
-        return i;
-    }
-
-    function deleteByValue(address toCheck, address[] storage addressList)
-    internal {
-        require(addressList.length > 0);
-        uint256 index = findIndex(toCheck, addressList);
-        if (index != addressList.length) {
-            addressList[index] = addressList[addressList.length - 1];
-            delete addressList[addressList.length - 1];
-        }
-    }
 }
 
--- truffle/contracts/Load/lib/SingleNotary.sol
+++ truffle/contracts/Load/lib/SingleNotary.sol
@@ -2,6 +2,9 @@ pragma solidity 0.5.0;
 
 library SingleNotary {
 
+    mapping(address => bool) private aclMapping;
+    address private vaultOwner;
+
     struct Data {
         string vaultHash;
         string vaultUri;
