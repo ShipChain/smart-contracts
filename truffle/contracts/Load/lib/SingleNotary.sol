f8a8959f98673da04afbdcc973d4aee174561f52
jianwel <ljw725@gmail.com>
2019-07-22 14:48:12 -0400

    removed extra events in library

--- truffle/contracts/Load/lib/SingleNotary.sol
+++ truffle/contracts/Load/lib/SingleNotary.sol
@@ -1,8 +1,6 @@
 pragma solidity 0.5.0;
 
 library SingleNotary {
-    //event VaultUri(address indexed msgSender, bytes16 indexed shipmentUuid, string vaultUri);
-    //event VaultHash(address indexed msgSender, bytes16 indexed shipmentUuid, string vaultHash);
 
     struct Data {
         string vaultHash;
