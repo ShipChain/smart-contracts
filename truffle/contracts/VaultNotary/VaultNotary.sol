3d1314c3c9a251e847f283129ccc24b035e7d49b
jianwel <ljw725@gmail.com>
2019-07-23 10:30:23 -0400

    removed the SingleNotary.sol imports and put it in a new folder

--- truffle/contracts/VaultNotary/VaultNotary.sol
+++ truffle/contracts/VaultNotary/VaultNotary.sol
@@ -1,9 +1,6 @@
 pragma solidity 0.5.0;
 
-
 import {Ownable} from "openzeppelin-solidity/contracts/ownership/Ownable.sol";
-import {SingleNotary} from "./lib/SingleNotary.sol";
-
 
 contract VaultNotary is Ownable {
 
