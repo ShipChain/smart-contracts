e9c30a39f089598afee4602d0940efbccf27bd30
jianwel <ljw725@gmail.com>
2019-12-19 15:52:32 -0500

    fixed the migration for profile

--- truffle/migrations/4_profile_contract.js
+++ truffle/migrations/4_profile_contract.js
@@ -1,14 +1,11 @@
-//var ERC725 = artifacts.require("ERC725");
-//var ERC735 = artifacts.require("ERC735");
-//var KeyHolder = artifacts.require("KeyHolder");
-//var ClaimHolder = artifacts.require("ClaimHolder");
+var ClaimHolder = artifacts.require("ClaimHolder");
 var ClaimVerifier = artifacts.require("ClaimVerifier");
 
 module.exports = function(deployer) {
   // Use deployer to state migration tasks.
-    //deployer.deploy(ERC725);
-    //deployer.deploy(ERC735);
-    //deployer.deploy(KeyHolder);
-    //deployer.deploy(ClaimHolder);
-    deployer.deploy(ClaimVerifier);
+    deployer.deploy(ClaimHolder).then(() => {
+        //print the address of the trustedClaimHolder if needed
+       // console.log(ClaimHolder.address);
+        deployer.deploy(ClaimVerifier(ClaimHolder.address));
+    })
 };
